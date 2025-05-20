1f8db2d23b11539e0cc1e760b1d69da5dcb0fab3
LinnyTPlayground <liinnny33@gmail.com>
2025-05-21 01:21:03 +0700

    feat(index.js): add yay, greeting, scolding Feature

--- index.js
+++ index.js
@@ -7,17 +7,48 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
+
 const greetings = ['hello', 'hi', 'hey', 'yo', 'sup'];
+
 
 client.on('messageCreate', message => {
     if (message.author.bot) return;
 
+    const username = message.member.displayName;
+    const responses = [`Hey ${username}!`, `Hi ${username}!`, `What's up ${username}!`, `Hello ${username}!`];
+    const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+
     const msg = message.content.toLowerCase();
 
-    if (msg === 'ping') {
-        message.channel.send('Pong!');
-    } else if (greetings.some(greet => msg.includes(greet))) {
-        message.channel.send('Hi there!');
+    // Yay Feature
+    if (msg.includes('yay')) {
+        message.channel.send(`Good Job ${username}!`)
+    }
+
+    // Greeting Feature
+    if (greetings.some(greet => msg.includes(greet))) {
+        message.channel.send(randomResponse);
+    }
+
+    // Scolding Feature
+    if (message.content.startsWith('!scold')) {
+        const mentionedUser = message.mentions.users.first();
+        if (!mentionedUser) {
+            message.reply('Please mention someone to scold (!scold @user)');
+            return;
+        };
+
+        const mentionedTag = `<@${mentionedUser.id}>`;
+        const scoldMessages = [
+            `Better stop lying and start apologizing ${mentionedTag}`,
+            `Prank together, get spanked together ${mentionedTag}`,
+            `${mentionedTag}... you're on thin ice.`,
+            `Behave yourself ${mentionedTag}`,
+            `Really? ${mentionedTag}`,
+            `That's not very nice ${mentionedTag}`,
+        ]
+        const randomScold = scoldMessages[Math.floor(Math.random() * scoldMessages.length)]
+        message.channel.send(randomScold);
     }
 });
 
